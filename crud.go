b997dd0209ef2b27113890360277be6faf705ecc
afnarqui <andresnaranjo@afnarqui.com>
2019-05-18 21:28:43 -0500

    connect with coakroachdb

--- crud.go
+++ crud.go
@@ -11,8 +11,40 @@ import (
 
 //connection should conect with bd
 func getConnection() *sql.DB {
-	dsn := "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
-	db, err := sql.Open("postgres", dsn)
+	//dsn := "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	db, err := sql.Open("postgres", "postgresql://maxroach@localhost:26257/test?sslmode=disable")
+	//db, err := sql.Open("postgres", dsn)
+
+	    if err != nil {
+        log.Fatal("error connecting to the database: ", err)
+    }
+
+    // Create the "accounts" table.
+    if _, err := db.Exec(
+        "CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)"); err != nil {
+        log.Fatal(err)
+    }
+
+    // Insert two rows into the "accounts" table.
+    if _, err := db.Exec(
+        "INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)"); err != nil {
+        log.Fatal(err)
+    }
+
+    // Print out the balances.
+    rows, err := db.Query("SELECT id, balance FROM accounts")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+    fmt.Println("Initial balances:")
+    for rows.Next() {
+        var id, balance int
+        if err := rows.Scan(&id, &balance); err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("%d %d\n", id, balance)
+    }
 
 	if err != nil {
 		log.Fatal(err)
