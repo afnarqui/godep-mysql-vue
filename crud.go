65949cb6b5a91764786ca070832f47401b9e20c0
afnarqui <andresnaranjo@afnarqui.com>
2019-05-18 20:06:48 -0500

    change26

--- crud.go
+++ crud.go
@@ -5,7 +5,22 @@ import (
 	"fmt"
 	"time"
 	"errors"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
+
+//connection should conect with bd
+func getConnection() *sql.DB {
+	dsn := "postgress://golang:golang@127.0.0.1:5432/gocrud?sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	return db
+}
+
 // struct student
 
 type Estudiante struct {
