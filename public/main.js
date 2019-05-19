6fa9be629528932ad53cdbf4680b0d223313a116
afnarqui <andresnaranjo@afnarqui.com>
2019-05-19 17:23:20 -0500

    chatv19

--- public/main.js
+++ public/main.js
@@ -1,2 +1,16 @@
 
 const socket = io();
+
+new Vue({
+    el: '$chat-app',
+    created() {
+
+    },
+    data: {
+        message: '',
+        messages: ['mensaje1','mensaje2','mensaje3']
+    },
+    methods: {
+
+    }
+})
