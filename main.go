0444a8049006bf0d1fb34d68593cd8c33623b7f4
afnarqui <andresnaranjo@afnarqui.com>
2019-05-20 00:07:43 -0500

    sqlitev12

--- main.go
+++ main.go
@@ -15,6 +15,15 @@ import (
 	"github.com/go-chi/chi"
 	*/
 )
+type Note struct {
+	ID          int       `json:"id,omitempty"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+}
+
+
 var note Note
 
 // Punto de ejecución del ejecutable.
