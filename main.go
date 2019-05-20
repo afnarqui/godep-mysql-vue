daf1ed69b2993a0d3f8d11461b1c3bc8180e42ca
afnarqui <andresnaranjo@afnarqui.com>
2019-05-20 09:04:37 -0500

    sqlitev69

--- main.go
+++ main.go
@@ -306,7 +306,7 @@ func Logger() http.Handler {
 
 var router *chi.Mux
 func routers() *chi.Mux {
-	router.Get("/notes", GetNotesHandler)
+	router.Get("/note", GetNotesHandler)
 	router.Post("/notes", CreateNotesHandler)
  /*   router.Get("/posts/{id}", DetailPost)
     
