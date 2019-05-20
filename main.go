e62b1aac0ae134becb72aebd95018001870a4988
afnarqui <andresnaranjo@afnarqui.com>
2019-05-19 19:57:59 -0500

    publicv4

--- main.go
+++ main.go
@@ -20,7 +20,37 @@ func main() {
 	filesDir := filepath.Join(workDir, "public")
 	FileServer(r, "/", http.Dir(filesDir))
 
+	r.Route("/", func(r chi.Router) {
+	
+		r.Get("/buscar", SearchArticles) // GET /articles/search
+
+	
+	})
+
 	http.ListenAndServe(":8081", r)
+}
+
+// Article fixture data
+var articles = []*Article{
+	{ID: "1", UserID: 100, Title: "Hi", Slug: "hi"},
+	{ID: "2", UserID: 200, Title: "sup", Slug: "sup"},
+	{ID: "3", UserID: 300, Title: "alo", Slug: "alo"},
+	{ID: "4", UserID: 400, Title: "bonjour", Slug: "bonjour"},
+	{ID: "5", UserID: 500, Title: "whats up", Slug: "whats-up"},
+}
+
+// SearchArticles searches the Articles data for a matching article.
+// It's just a stub, but you get the idea.
+func buscar(w http.ResponseWriter, r *http.Request) {
+	render.RenderList(w, r, NewArticleListResponse(articles))
+}
+
+func NewArticleListResponse(articles []*Article) []render.Renderer {
+	list := []render.Renderer{}
+	for _, article := range articles {
+		list = append(list, NewArticleResponse(article))
+	}
+	return list
 }
 
 // FileServer conveniently sets up a http.FileServer handler to serve
