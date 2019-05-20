8bea412969039989e76a940deb3fb687a375b787
afnarqui <andresnaranjo@afnarqui.com>
2019-05-19 19:14:47 -0500

    chav39Br

--- main.go
+++ main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"github.com/googollee/go-socket.io"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("New user connected:", s.ID())
-
-		return nil
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
 	})
-
-
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-	server.OnEvent("chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		//return "recv " + msg
-	
-		return msg
-	})
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-	server.OnError("/", func(e error) {
-		fmt.Println("meet error:", e)
-	})
-	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
-		fmt.Println("closed", msg)
-	})
-	go server.Serve()
-	defer server.Close()
-
-	// http
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Println("Serving at localhost:8081...")
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+	http.ListenAndServe(":8081", r)
 }
