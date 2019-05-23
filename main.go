303b64675e673ad42f3f3f7a7ce9f83635408f1e
afnarqui <andresnaranjo@afnarqui.com>
2019-05-23 09:49:10 -0500

    SQLV1

--- main.go
+++ main.go
@@ -62,24 +62,83 @@ func GetConnection() *sql.DB {
 		if _, err := db.Exec(
 			`CREATE TABLE IF NOT EXISTS domaintest (
 					title VARCHAR(64) NULL,
-					datas             VARCHAR(8000)
+					description VARCHAR(200) NULL,
+					Uuid VARCHAR(350),
+					Host VARCHAR(120),
+					Port INT,
+					Protocol VARCHAR(120),
+					IsPublic BOOL,
+					Status   VARCHAR(80),
+					StartTime       TIMESTAMP,
+					TestTime        INT,
+					EngineVersion   VARCHAR(120),
+					CriteriaVersion VARCHAR(120),
+					Endpoints       VARCHAR(8000) NULL,
+					HostNew         VARCHAR(8000) NULL,
+					HostOld         VARCHAR MAX() NULL
 				)`); err != nil {
 			log.Fatal(err)
 		}
 
 		if _, err := db.Exec(
 			`INSERT INTO domaintest (
-					title,datas
-				) VALUES ('ANDRES','{"datas": {"nombre": "github.com"}}')`); err != nil {
+					Host,
+					Port,
+					Protocol, 
+					IsPublic,
+					Status,   
+					StartTime,
+					TestTime ,
+					EngineVersion,   
+					CriteriaVersion,
+					endpoints
+				) VALUES (
+					'www.google.com',
+					443,
+					'http',
+					false,
+					'READY',
+					1558556345528,
+					1558556526417,
+					'1.34.2',
+					'2009p',
+					'{"endpoints": [
+						{
+						"ipAddress": "2607:f8b0:4005:809:0:0:0:2004",
+						"serverName": "sfo03s08-in-x04.1e100.net",
+						"statusMessage": "Ready",
+						"grade": "A+",
+						"gradeTrustIgnored": "A+",
+						"hasWarnings": false,
+						"isExceptional": true,
+						"progress": 100,
+						"duration": 85620,
+						"delegation": 2
+						},
+						{
+						"ipAddress": "172.217.6.36",
+						"serverName": "sfo03s08-in-f4.1e100.net",
+						"statusMessage": "Ready",
+						"grade": "A+",
+						"gradeTrustIgnored": "A+",
+						"hasWarnings": false,
+						"isExceptional": true,
+						"progress": 100,
+						"duration": 95185,
+						"delegation": 2
+						}
+				  	]}')`); err != nil {
 			log.Fatal(err)
 		}
 
-		if _, err := db.Exec(
-			`INSERT INTO domaintest (
-					title,datas
-				) VALUES ('FELIPE','[{"nombre":"andres felipe naranjo quintero new"}]')`); err != nil {
-			log.Fatal(err)
-		}
+		
+
+		// if _, err := db.Exec(
+		// 	`INSERT INTO domaintest (
+		// 			title,datas
+		// 		) VALUES ('FELIPE','[{"nombre":"andres felipe naranjo quintero new"}]')`); err != nil {
+		// 	log.Fatal(err)
+		// }
 
 		// if _, err := db.Exec(
 		// 	`INSERT INTO domain (
